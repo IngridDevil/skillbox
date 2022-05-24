a99130b3f0839305d27e473e3bbcab286c4f3690
IngridDevil <ns.shmakova97@gmail.com>
2022-05-24 10:53:10 +0300

    Заменила изображения

--- src/data/products.js
+++ src/data/products.js
@@ -2,61 +2,61 @@ export default [
     {
         title: "Беспроводные наушники Sony WH-1000XM4",
         price: 34600,
-        image_url: "/img/new_images/headphone_sony.jpeg"
+        image_url: "/img/new_images/headphone_sony.jpg"
     },
     {
         title: "Беспроводные наушники JBL Tune 710BT",
         price: 6190,
-        image_url: "/img/new_images/headphone_jbl.jpeg"
+        image_url: "/img/new_images/headphone_jbl.jpg"
     },
     {
         title: "Беспроводные наушники Marshall Major IV",
         price: 15290,
-        image_url: "/img/new_images/headphone_marshall.jpeg"
+        image_url: "/img/new_images/headphone_marshall.jpg"
     },
     {
         title: "Смартфон Apple iPhone 13",
         price: 97000,
-        image_url: "/img/new_images/phone_apple.jpeg"
+        image_url: "/img/new_images/phone_apple.jpg"
     },
     {
         title: "Смартфон Samsung Galaxy S21",
         price: 69000,
-        image_url: "/img/new_images/phone_samsung.jpeg"
+        image_url: "/img/new_images/phone_samsung.jpg"
     },
     {
         title: "Смартфон OnePlus Nord 2",
         price: 46900,
-        image_url: "/img/new_images/phone_oneplus.jpeg"
+        image_url: "/img/new_images/phone_oneplus.jpg"
     },
     {
         title: "Ноутбук ASUS TUF Gaming F15",
         price: 195000,
-        image_url: "/img/new_images/laptop_asus.jpeg"
+        image_url: "/img/new_images/laptop_asus.jpg"
     },
     {
         title: "Ноутбук MSI Pulse",
         price: 115000,
-        image_url: "/img/new_images/laptop_msi.jpeg"
+        image_url: "/img/new_images/laptop_msi.jpg"
     },
     {
         title: "Ноутбук Xiaomi Redmibook Pro 15",
         price: 76900,
-        image_url: "/img/new_images/laptop_xiaomi.jpeg"
+        image_url: "/img/new_images/laptop_xiaomi.jpg"
     },
     {
         title: "Фотоаппарат Canon EOS 250D",
         price: 60900,
-        image_url: "/img/new_images/camera_canon.jpeg"
+        image_url: "/img/new_images/camera_canon.jpg"
     },
     {
         title: "Фотоаппарат Panasonic Lumix DC-G90",
         price: 89900,
-        image_url: "/img/new_images/camera_lumix.jpeg"
+        image_url: "/img/new_images/camera_lumix.jpg"
     },
     {
         title: "Фотоаппарат Nikon D750",
         price: 46900,
-        image_url: "/img/new_images/camera_nikon.jpeg"
+        image_url: "/img/new_images/camera_nikon.jpg"
     },
 ]
